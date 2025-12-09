04e0fc15bdf2971e12f4a35b721a8740f2cf3811
joungbinlee <joungbinlee@kaist.ac.kr>
2025-12-09 13:03:38 +0000

    first commit

--- Depth-Anything-3/dynamic_mask_point_tracking.py
+++ Depth-Anything-3/dynamic_mask_point_tracking.py
@@ -1,13 +1,4 @@
 import os
-PATH = '/mnt/cache' # in the case of singularity setup
-os.environ['TRANSFORMERS_CACHE'] = PATH
-os.environ['HF_HOME'] = PATH
-os.environ['HF_DATASETS_CACHE'] = PATH
-os.environ['TORCH_HOME'] = PATH
-
-
-
-import os
 import numpy as np
 import torch
 import time
